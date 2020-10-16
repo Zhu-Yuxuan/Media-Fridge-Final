3c33deacdef313ea6f3c080a1719de74098b1042
“Zhu-Yuxuan” <zyx-max-sjtu@sjtu.edu.cn>
2020-10-17 04:08:05 +0800

    thread error fix

--- GUI.py
+++ GUI.py
@@ -159,8 +159,10 @@ def terminal_simu(fridge):
 if __name__ == "__main__":
     fridge = [Tank(), Tank(), Tank(), Tank(), Tank(), Tank()]
     t1 = threading.Thread(target=fridgeUI)
-    t2 = threading.Thread(target=logic0, args=("fridge[0]",))
-    t3 = threading.Thread(target=logic1, args=("fridge[1]",))
+    t2 = threading.Thread(target=logic0, args=(fridge[0],))
+    t3 = threading.Thread(target=logic1, args=(fridge[1],))
     t4 = threading.Thread(target=detection.detect)
     t1.start()
     t2.start()
+    t3.start()
+    t4.start()
